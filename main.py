3edcbd0d6f25aac91df473f41b13327c1a0974a3
Serov Ivan <serovvans@yandex.ru>
2023-07-11 22:30:38 +0300

    import get_numbers_diff in main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from funcs import get_numbers_sum
+from funcs import get_numbers_sum, get_numbers_diff
 
 
 if __name__ == "__main__":
     print(get_numbers_sum(25, 13743))
-
+    print(get_numbers_diff(234525, 13))
